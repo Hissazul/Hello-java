69259bb12ddf6932f5c4d844e64c4dd7650bfbd9
atomic <ilyaklimof@gmail.com>
2025-03-25 16:14:27 +0300

    Fourth commit "branch1"

--- LessonGit/src/Main.java
+++ LessonGit/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to java!");
         System.out.println("This way please!");
+        System.out.println("Branch1");
     }
 }
